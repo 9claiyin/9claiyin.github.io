7a0af58c4a72606a99ad50825b3294bd4f42c4b5
cuicang <2026871869@qq.com>
2025-01-27 17:51:00 +0800

    Site updated: 2025-01-27 17:50:59

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/06/17/hello-world/","2025/01/16/test1/"];function toRandomPost(){
+var posts=["2025/01/16/test1/","2024/06/17/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
